c9bc9fb71b29465c0e6852a4cc51d37e3a43ef86
joseph chibuike <124733643+larchky@users.noreply.github.com>
2025-06-10 13:43:37 +0100

    Create train.py

---
+++ train_model/train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+import zipfile
+import os
+
+#path to yaml file
+yaml_path = "armed--2.yaml"
+#path to model
+model_path = "/yolo11s-pose.pt"
+
+model = YOLO('model_path')
+model.train(data=yaml_path,  epochs=100, imgsz=704, project = '/content/drive/MyDrive/gun_detection',
+            name = 'experiment')
