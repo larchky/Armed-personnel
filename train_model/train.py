4b244fcc9d47b3e6968063a6665088f4fab83a5d
joseph chibuike <124733643+larchky@users.noreply.github.com>
2025-06-10 16:37:34 +0100

    Update train.py

--- train_model/train.py
+++ train_model/train.py
@@ -6,7 +6,7 @@ import os
 #path to yaml file
 yaml_path = "armed--2.yaml"
 #path to model
-model_path = "yolo11s-pose.pt"
+model_path = "yolo11s.pt"
 
 model = YOLO(model_path)
 model.train(data=yaml_path,  epochs=100, imgsz=704, project = 'gun_detection',
